7aecf532b02fbb9acaa457292fef6c9b7b8beae6
Yannis Douros <yannis@douros.org>
2022-03-14 16:16:00 +0200

    Slightly animate the drawing process.

--- spirograph.py
+++ spirograph.py
@@ -3,18 +3,19 @@ import numpy as np
 import math
 import time
 
-img_rows = 480
-img_cols = 640
+img_rows = 800
+img_cols = 800
 img_channels = 3
 
 spire_length = 50000
 spire_step = 0.01
 
-r1 = 100
-r2 = -30 # >0 for hypotrochoid, >0 for epitrochoid
-r3 = 50
-color = (0, 0, 255)
+r1 = 250
+r2 = 48 # >0 for hypotrochoid, >0 for epitrochoid
+r3 = 25
+color = (255, 0, 0)
 thickness = 1
+pace = 1
 
 def spire(canvas):
     row_shift = img_rows / 2.0
@@ -31,13 +32,14 @@ def spire(canvas):
         y = col_shift + (fr1 - fr2) * math.sin(t) - fr3 * math.sin((fr1-fr2)/fr2 * t)
         end_point = (int(y), int(x))
         if(i > 0):
-            canvas = cv2.line(canvas, start_point, end_point, color, thickness)
+            canvas = cv2.line(canvas, start_point, end_point, (i%255,128,128), thickness)
         start_point = end_point
+        cv2.imshow('RGB', canvas)
+        cv2.waitKey(pace)
 
 def main():
     canvas = np.full((img_rows, img_cols, img_channels), fill_value = 255, dtype = np.uint8)
     spire(canvas)
-    cv2.imshow('RGB', canvas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
