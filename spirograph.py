14c93bd07632e2b068dca87fe3620cb862a07825
Yannis Douros <yannis@douros.org>
2022-03-31 18:44:39 +0300

    Anti-aliasing, and show the circles.

--- spirograph.py
+++ spirograph.py
@@ -7,16 +7,16 @@ img_rows = 800
 img_cols = 800
 img_channels = 3
 
-spire_length = 5000
+spire_length = 500000
 spire_step = 0.01
 
 # The next three must be FLOAT
-r_fixed = 300.0     # radius of the fixed circle
-r_rolling = 25.0   # radius of the rolling circle, >0 for hypotrochoid, >0 for epitrochoid
-d = 100.0           # distance of tracing point from the centre of the rolling circle
-color = (255, 0, 0)
-thickness = 1
-pace = 1
+r_fixed = 390.0     # radius of the fixed circle
+r_rolling = 310.0   # radius of the rolling circle, >0 for hypotrochoid, >0 for epitrochoid
+d = 170.0           # distance of tracing point from the centre of the rolling circle
+color = (0, 255, 0)
+thickness = 2
+pace = 0
 
 def spire(canvas):
     row_shift = img_rows / 2.0
@@ -33,14 +33,25 @@ def spire(canvas):
         y = col_shift + (d_r) * math.sin(t) - d * math.sin((d_r / r_rolling) * t)
         end_point = (int(y), int(x))
         if(i > 0):
-            canvas = cv2.line(canvas, start_point, end_point, (i%255,128,128), thickness)
+            canvas = cv2.line(canvas, start_point, end_point, color, thickness, cv2.LINE_AA)
         start_point = end_point
-        cv2.imshow('RGB', canvas)
-        cv2.waitKey(pace)
+        if pace > 0:
+            cv2.imshow('RGB', canvas)
+            cv2.waitKey(pace)
+
+def show_circles(canvas):
+    circle_color = (0, 0, 255)
+    canvas = cv2.circle(canvas, (int(img_cols/2), int(img_rows/2)), int(r_fixed), circle_color)
+    canvas = cv2.circle(canvas, (int(img_cols/2), int(img_rows/2 - r_fixed + r_rolling)), int(r_rolling), circle_color)
+    cv2.imshow('RGB', canvas)        
 
 def main():
     canvas = np.full((img_rows, img_cols, img_channels), fill_value = 255, dtype = np.uint8)
     spire(canvas)
+    if pace == 0:
+        cv2.imshow('RGB', canvas)
+    cv2.waitKey(0)
+    show_circles(canvas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
